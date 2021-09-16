95cbc758709b7a854d30d15d7f16ea879d86c0f6
ferrerinicolas <89610462+ferrerinicolas@users.noreply.github.com>
2021-09-16 17:56:36 +0200

    First printing in python

---
+++ Printing in python.py
@@ -0,0 +1,32 @@
+print ("Hello World")
+
+
+print ("This is a line of text!")
+print ("Here's another line.")
+
+#Do it for yoursel
+print ("My name is Nicolas Sebastián.")
+print("Im learning to programming in Python, and i like it.")
+
+
+"""In the editor is a buggy program for the problem outlined below. Find the bug and fix it.
+
+The program in this exercise doesn’t run! It looks like this:"""
+
+print("Hi there!")
+#print(My favorite color is magenta.)  ADD ""
+#Try to run it, and see what happens. Then see if you can fix it.
+
+"""In this exercise, you will write a program that displays your name written vertically.
+
+For example, if your name was Maria, the output should be:""""
+
+print("N")
+print("i")
+print("c")
+print("o")
+print("l")
+print("a")
+print("s")
+
+#FINISHED
