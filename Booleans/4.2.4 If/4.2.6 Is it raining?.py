d463c8405042ceeed38c2c805867140c898e85f9
ferrerinicolas <89610462+ferrerinicolas@users.noreply.github.com>
2021-09-20 03:52:08 +0200

    AA

---
+++ Booleans/4.2.4 If/4.2.6 Is it raining?.py
@@ -0,0 +1,17 @@
+"""Write a program that has a Boolean variable that represents whether 
+or not it is raining outside.
+
+
+Your program should 
+
+print I'm going to dance in the rain! if it is raining, and 
+I'm going to dance in the sun! if it is not raining."""
+
+
+its_raining = False
+
+if its_raining:
+    print ("I'm going to dance in the rain! if it is raining")
+    
+else:
+    print ("I'm going to dance in the sun! if it is not raining.")
