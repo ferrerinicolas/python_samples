22c1c7f838f28358041452f59b2d2cefdd0775d6
ferrerinicolas <89610462+ferrerinicolas@users.noreply.github.com>
2021-09-26 02:32:50 +0200

    Yes

--- 6. Functions/6.2/6.2.8 Area of a square.py
+++ 6. Functions/6.2/6.2.8 Area of a square.py
@@ -1,14 +1,16 @@
 #Def function to print the calculated area of the square
-def calculate_area(side_length):
-    print("The area of the square it is: " + str(side_length * side_length))
-
+def calculate_area(side_length=10):
+    area = side_length * side_length
+    
+    print("The area of a square with sides of length " + str(side_length) + " is " + str(area))
+    
 #Input of user for the side length
 user_length = int ( input ("Enter side length: "))
 
 #if user length is less or equal than 0, use 10 as predeterminated
 if user_length <= 0:
-    calculate_area(10)
-    
+    calculate_area()
+
 #Else use the value given by the user
 else:
     calculate_area(user_length)
